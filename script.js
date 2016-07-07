0740a52c43f152f5b6e78f169fd8220df85fed58
citygent <cyberyt@gmail.com>
2016-07-07 16:42:59 +0100

    have range slider ish

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-$(document).ready(function() {
-	var $ = window.jQuery
+/* global $*/
+$(document).ready(function () {
+// init the slider for step 1 - budget
+  $('.slider-container').slider({
+    range: true,
+    max: 5000,
+    min: 0,
+    values: [500, 100],
+    slide: function (event, ui) {
+      $('#budget-amount').val('$' + ui.values[ 0 ] + ' - $' + ui.values[1])
+    }
+  })
+  $('#budget-amount').val('$' + $('.slider-container').slider('values', 0) + ' - $' + $('.slider-container').slider('values', 1))
 
-	//init the slider for step 1 - budget
-	$('.slider-container').slider();
-});
+})
